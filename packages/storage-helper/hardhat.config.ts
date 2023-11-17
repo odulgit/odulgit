72e60728857e5c7f008a2804c2a9b6d9e09f33d3
kidneyweak <kidneyweak@gmail.com>
2023-11-18 01:19:12 +0300

    feat: filecoin upload basic method

--- packages/storage-helper/hardhat.config.ts
+++ packages/storage-helper/hardhat.config.ts
@@ -1,60 +1,44 @@
-import { HardhatUserConfig } from 'hardhat/config'
-import { NetworkUserConfig } from 'hardhat/types'
+import { HardhatUserConfig } from "hardhat/config"
+import { NetworkUserConfig } from "hardhat/types"
 // hardhat plugin
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomicfoundation/hardhat-toolbox'
+import "hardhat-deploy"
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomicfoundation/hardhat-toolbox"
 
-import { config as dotenvConfig } from 'dotenv'
-import { resolve } from 'path'
-import { loadTasks } from '../git-contract/scripts/helpers/hardhatConfigHelpers'
+import { config as dotenvConfig } from "dotenv"
+import { resolve } from "path"
+import { loadTasks } from "./scripts/helpers/hardhatConfigHelpers"
 
-dotenvConfig({ path: resolve(__dirname, './.env') })
+dotenvConfig({ path: resolve(__dirname, "./.env") })
 
-const taskFolder = ['tasks']
+const taskFolder = ["tasks"]
 loadTasks(taskFolder)
 
 const chainIds = {
-  ganache: 1337,
-  goerli: 5,
-  sepolia: 11155111,
   hardhat: 31337,
-  quorum: 81712,
-  mainnet: 1,
-  avalanche: 43114,
-  bsc: 56,
-  'arbitrum-mainnet': 42161,
-  'polygon-mainnet': 137,
-  'optimism-goerli': 420,
-  'optimism-mainnet': 10,
-  'polygon-mumbai': 80001,
+  calibration: 314159,
+  filecoinMainnet: 314,
 }
 
 // Ensure that we have all the environment variables we need.
-const pk: string | undefined = process.env.PRIVATE_KEY
+let pk: string | undefined = process.env.PRIVATE_KEY
 if (!pk) {
-  throw new Error('Please set your pk in a .env file')
-}
-
-const infuraApiKey: string | undefined = process.env.INFURA_API_KEY
-if (!infuraApiKey) {
-  throw new Error('Please set your INFURA_API_KEY in a .env file')
+  pk = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaba"
+  // throw new Error("Please set your pk in a .env file")
 }
 
 function getChainConfig (chain: keyof typeof chainIds): NetworkUserConfig {
   let jsonRpcUrl: string
   switch (chain) {
-    case 'avalanche':
-      jsonRpcUrl = 'https://api.avax.network/ext/bc/C/rpc'
+    case "calibration":
+      jsonRpcUrl = "https://api.calibration.node.glif.io/rpc/v1"
       break
-    case 'optimism-goerli':
-      jsonRpcUrl = 'https://goerli.optimism.io'
-      break
-    case 'quorum':
-      jsonRpcUrl = process.env.QUORUM_URL || ''
+    case "filecoinMainnet":
+      jsonRpcUrl = "https://api.node.glif.io"
       break
     default:
-      jsonRpcUrl = `https://${chain}.infura.io/v3/${infuraApiKey}`
+      jsonRpcUrl = "https://api.calibration.node.glif.io/rpc/v1"
   }
   return {
     accounts: [`0x${pk}`],
@@ -64,87 +48,32 @@ function getChainConfig (chain: keyof typeof chainIds): NetworkUserConfig {
 }
 
 const config: HardhatUserConfig = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: chainIds.hardhat,
     },
     local: {
-      url: 'http://127.0.0.1:8545',
+      url: "http://127.0.0.1:8545",
     },
-    arbitrum: getChainConfig('arbitrum-mainnet'),
-    avalanche: getChainConfig('avalanche'),
-    bsc: getChainConfig('bsc'),
-    goerli: getChainConfig('goerli'),
-    sepolia: getChainConfig('sepolia'),
-    mainnet: getChainConfig('mainnet'),
-    optimism: getChainConfig('optimism-mainnet'),
-    quorum: getChainConfig('quorum'),
-    'optimism-goerli': getChainConfig('optimism-goerli'),
-    'polygon-mainnet': getChainConfig('polygon-mainnet'),
-    'polygon-mumbai': getChainConfig('polygon-mumbai'),
+    calibration: getChainConfig("calibration"),
+    filecoinMainnet: getChainConfig("filecoinMainnet"),
   },
   paths: {
-    artifacts: './artifacts',
-    cache: './cache',
-    sources: './contracts',
-    tests: './test',
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
   },
   solidity: {
-    compilers: [
-      {
-        version: '0.8.18',
-      },
-    ],
+    version: "0.8.17",
     settings: {
-      metadata: {
-        // Not including the metadata hash
-        // https://github.com/paulrberg/hardhat-template/issues/31
-        bytecodeHash: 'none',
-      },
-      // Disable the optimizer when debugging
-      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
         enabled: true,
-        runs: 800,
+        runs: 1000,
+        details: { yul: false },
       },
     },
-  },
-  etherscan: {
-    apiKey: {
-      arbitrumOne: process.env.ARBISCAN_API_KEY || '',
-      avalanche: process.env.SNOWTRACE_API_KEY || '',
-      bsc: process.env.BSCSCAN_API_KEY || '',
-      goerli: process.env.ETHERSCAN_API_KEY || '',
-      sepolia: process.env.ETHERSCAN_API_KEY || '',
-      mainnet: process.env.ETHERSCAN_API_KEY || '',
-      optimisticEthereum: process.env.OPTIMISM_API_KEY || '',
-      polygon: process.env.POLYGONSCAN_API_KEY || '',
-      optimisticGoerli: process.env.OPTIMISM_API_KEY || '',
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY || '',
-      quorum: 'NO_API_KEY',
-    },
-    customChains: [{
-      network: 'quorum',
-      chainId: chainIds.quorum,
-      urls: {
-        apiURL: `${process.env.BLOCKSCOUT_URL}/api`,
-        browserURL: process.env.BLOCKSCOUT_URL as string,
-      },
-    }],
-
-  },
-  
-  gasReporter: {
-    currency: 'USD',
-    gasPrice: 100,
-    enabled: process.env.REPORT_GAS as string === 'true',
-    excludeContracts: [],
-    src: './contracts',
-  },
-  typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v5',
   },
 }
 
---
+++ packages/storage-helper/src/filecoin/deal.ts
@@ -0,0 +1,53 @@
+import { ethers } from "ethers"
+import CID from "cids"
+
+export const dealClient = async (taskArgs: any, network: any) => {
+  const cid = taskArgs.pieceCid
+  const cidHexRaw = new CID(cid).toString("base16").substring(1)
+  const cidHex = `0x${cidHexRaw}`
+  const contractAddr = taskArgs.contract
+
+  const verified = (taskArgs.verifiedDeal === "true")
+  const skipIpniAnnounce = (taskArgs.skipIpniAnnounce === "true")
+  const removeUnsealedCopy = (taskArgs.removeUnsealedCopy === "true")
+
+  const extraParamsV1 = [
+    taskArgs.locationRef,
+    taskArgs.carSize,
+    skipIpniAnnounce,
+    removeUnsealedCopy,
+  ]
+
+  const DealRequestStruct = [
+    cidHex,
+    taskArgs.pieceSize,
+    verified,
+    taskArgs.label,
+    taskArgs.startEpoch,
+    taskArgs.endEpoch,
+    taskArgs.storagePricePerEpoch,
+    taskArgs.providerCollateral,
+    taskArgs.clientCollateral,
+    taskArgs.extraParamsVersion,
+    extraParamsV1,
+  ]
+  const networkId = network.name
+  console.log("Making deal proposal on network", networkId)
+
+  // create a new wallet instance
+  const wallet = new ethers.Wallet(network.config.accounts[0], ethers.provider)
+
+  // create a DealClient contract factory
+  const DealClient = await ethers.getContractFactory("DealClient", wallet)
+  // create a DealClient contract instance
+  // this is what you will call to interact with the deployed contract
+  const dealClient = await DealClient.attach(contractAddr)
+
+  // send a transaction to call makeDealProposal() method
+  const transaction = await dealClient.makeDealProposal(DealRequestStruct)
+  const transactionReceipt = await transaction.wait()
+
+  // listen for DealProposalCreate event
+  const event = transactionReceipt.events[0].topics[0]
+  console.log("Complete! Event Emitted. ProposalId is:", event)
+}
