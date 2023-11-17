9e00a5a1dc91fe835e85675da77211f834f4ec25
kidneyweak <kidneyweak@gmail.com>
2023-11-17 22:31:56 +0300

    feat: odul yargs infra

---
+++ apps/odul/src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import yargs from 'yargs'
+
+const usageText = `
+odulgit - a bounty git tool
+`
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const argv = yargs
+  .commandDir('lib')
+  .strict()
+  .demandCommand()
+  .completion()
+  .usage(usageText)
+  .argv
