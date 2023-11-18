35a78a9fe76bf4431298f85022c82393ef17aae2
kidneyweak <kidneyweak@gmail.com>
2023-11-18 12:20:36 +0300

    feat: storage adjust

--- packages/storage-helper/scripts/helpers/pathHelper.ts
+++ packages/storage-helper/scripts/helpers/pathHelper.ts
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs"
+import path from "path"
 
 export const writeFileSync = (
   dirPath: string,
@@ -7,5 +7,5 @@ export const writeFileSync = (
   data: any,
 ) => {
   fs.mkdirSync(dirPath, { recursive: true })
-  fs.writeFileSync(path.join(dirPath, fileName), data, 'utf8')
+  fs.writeFileSync(path.join(dirPath, fileName), data, "utf8")
 }
